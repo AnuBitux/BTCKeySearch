13996450f16148810cd47e517cd889e6f2663146
AnuBitux <122746698+AnuBitux@users.noreply.github.com>
2024-03-24 22:28:31 +0100

    Update btckeysearch.sh

--- btckeysearch.sh
+++ btckeysearch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo ' '
+figlet BTCKeySearch
+echo ' '
 echo btckeysearch.py is a python tool that scans all the files in a directory,
 echo 'including subdirectories, looking for Bitcoin private keys, BIP32 root keys (xpriv) and Monero private spend keys.'
 echo ''
